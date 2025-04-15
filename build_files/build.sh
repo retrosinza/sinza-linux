6a3483aca34bef017326f5df8157e384506f9380
sinza <sinza@sdf.org>
2025-04-15 19:41:46 -0400

    disabled 86box building

--- build_files/build.sh
+++ build_files/build.sh
@@ -34,13 +34,13 @@ dnf5 group install -y --with-optional virtualization
 # dnf5 -y copr disable ublue-os/staging
 
 # Installs 86box
-dnf5 -y copr enable rob72/86Box
-dnf5 install -y 86Box
-mkdir -p /usr/local/share/86Box/
-curl -sL https://github.com/86Box/roms/archive/refs/tags/$BOX86_VERSION.tar.gz -o /usr/local/share/86Box/$BOX86_VERSION.tar.gz
-tar xzf /usr/local/share/86Box/$BOX86_VERSION.tar.gz
-mv $BOX86_VERSION-roms roms
-dnf5 -y copr disable rob72/86Box #disables COPR so it doesn't end up on final image
+# dnf5 -y copr enable rob72/86Box
+# dnf5 install -y 86Box
+# mkdir -p /usr/local/share/86Box/
+# curl -sL https://github.com/86Box/roms/archive/refs/tags/$BOX86_VERSION.tar.gz -o /usr/local/share/86Box/$BOX86_VERSION.tar.gz
+# tar xzf /usr/local/share/86Box/$BOX86_VERSION.tar.gz
+# mv $BOX86_VERSION-roms roms
+# dnf5 -y copr disable rob72/86Box #disables COPR so it doesn't end up on final image
 
 #### Example for enabling a System Unit File
 
