2df4e6b1f5394e9c637ac5d6745e8f7a437e6aba
sinza <sinza@sdf.org>
2025-04-15 18:58:18 -0400

    set curl to actually give the downloaded file a name 🤦

--- build_files/build.sh
+++ build_files/build.sh
@@ -39,7 +39,7 @@ dnf5 install -y @workstation-ostree-support install 'dnf5-command(copr)'
 dnf5 -y copr enable rob72/86Box
 dnf5 install -y 86Box
 mkdir -p /usr/local/share/86Box/
-curl -sL ""https://github.com/86Box/roms/archive/refs/tags/$BOX86_VERSION.tar.gz -o /usr/local/share/86Box/
+curl -sL ""https://github.com/86Box/roms/archive/refs/tags/$BOX86_VERSION.tar.gz -o /usr/local/share/86Box/$BOX86_VERSION.tar.gz
 tar xzf /usr/local/share/86Box/$BOX86_VERSION.tar.gz
 mv $BOX86_VERSION-roms roms
 dnf5 -y copr disable rob72/86Box #disables COPR so it doesn't end up on final image
