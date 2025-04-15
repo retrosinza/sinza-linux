cd796b95167530ecaf7594c5907084a295cff791
sinza <sinza@sdf.org>
2025-04-15 16:34:26 -0400

    added packages for testing image

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+86BOX_VERSION="v4.2.1"
+
 set -ouex pipefail
 
 ### Install packages
@@ -9,12 +11,19 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 remove -y firefox thunderbird toolbox
+# Installs workstation components
+dnf5 install -y workstation-product workstation-ostree-support
+
+# removes toolbox and fedora flathub
+dnf5 remove -y toolbox fedora-flathub-remote
 dnf5 install -y distrobox # dont remove deps shared between distrobox and toolbox
 dnf5 autoremove -y
 
+# installs tui browsers because I like them
+dnf5 install -y @text-internet
+
 # this installs the whole virtualization group
+# the --with-optional version includes all architectures supported by QEMU
 dnf5 group install -y --with-optional virtualization
 
 # Use a COPR Example:
@@ -23,6 +32,15 @@ dnf5 group install -y --with-optional virtualization
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+
+# Installs 86box
+dnf5 -y copr enable rob72/86box
+dnf5 install -y 86box
+mkdir -p /usr/local/share/86Box/
+curl -sL https://github.com/86Box/roms/archive/refs/tags/$($86BOX_VERSION).tar.gz -o /usr/local/share/86Box/
+tar xzf /usr/local/share/86Box/$($86BOX_VERSION).tar.gz
+mv $($86BOX_VERSION)-roms roms
+dnf5 -y copr disable rob72/86box #disables COPR so it doesn't end up on final image
 
 #### Example for enabling a System Unit File
 
