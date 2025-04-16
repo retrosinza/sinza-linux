a1a08e9013a255da238f613c25724a692281e742
sinza <sinza@sdf.org>
2025-04-16 12:39:43 -0400

    changed changed url from 39 to 42, the latest fedora

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ set -ouex pipefail
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/42/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # Installs workstation components
 dnf5 install -y @workstation-product @workstation-ostree-support
